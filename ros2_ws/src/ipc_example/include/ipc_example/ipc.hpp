f41f48e1a220aee9e941dc2fae95350ebaeb7121
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2025-04-25 15:03:53 -0300

    ROS2 node working as subscriber

--- ros2_ws/src/ipc_example/include/ipc_example/ipc.hpp
+++ ros2_ws/src/ipc_example/include/ipc_example/ipc.hpp
@@ -4,7 +4,24 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-#include <string>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+// Socket includes
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define SERVER_IP "143.106.207.65"
+
+typedef struct socket_msg
+{
+    char msg[100];
+    int code;
+} socket_msg_t;
 
 namespace ipc
 {
@@ -15,7 +32,11 @@ namespace ipc
         ~IpcExample();
 
     private:
-       
+        int server_fd, rx_socket;
+        struct sockaddr_in address;
+        int addrlen;
+        socket_msg_t buffer;
+        socket_msg_t msg;
 
         void msg_timer_callback();
 
--- ros2_ws/src/ipc_example/src/ipc.cpp
+++ ros2_ws/src/ipc_example/src/ipc.cpp
@@ -7,6 +7,36 @@ namespace ipc
         msg_pub_ = this->create_publisher<std_msgs::msg::String>("/icp_msg", 1);
         msg_timer_ = this->create_wall_timer(std::chrono::seconds(1),
                                              std::bind(&IpcExample::msg_timer_callback, this));
+
+        addrlen = sizeof(address);
+        msg = {"Hello from server", 1024};
+
+        // Create socket
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Bind the socket
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY; // Bind to any interface
+        address.sin_port = htons(PORT);
+
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+        {
+            perror("bind failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Listen for a connection
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
     }
 
     IpcExample::~IpcExample()
@@ -15,11 +45,29 @@ namespace ipc
 
     void IpcExample::msg_timer_callback()
     {
+        printf("Waiting for a connection...\n");
+
+        // Accept a connection
+        if ((rx_socket = accept(server_fd, (struct sockaddr *)&address,
+                                (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Receiving
+        read(rx_socket, &buffer, sizeof(buffer));
+        printf("Client msg : %s\n", buffer.msg);
+        printf("Client code: %d\n", buffer.code);
+
+        // Sending
+        send(rx_socket, &msg, sizeof(msg), 0);
+        printf("msg sent to client\n");
 
         std_msgs::msg::String msg;
-        msg.data = std::string(shm_buf->string);
-        RCLCPP_INFO(this->get_logger(), "Publishing IPC message | %d", shm_buf->size);
-        RCLCPP_INFO(this->get_logger(), shm_buf->string);
+        msg.data = std::string(buffer.msg);
+        RCLCPP_INFO(this->get_logger(), "Publishing IPC message | %d", buffer.code);
+        RCLCPP_INFO(this->get_logger(), buffer.msg);
         msg_pub_->publish(msg);
     }
 
